01965b2f0e6b7bc41c37120d3b01a0b5f3c83c2d
David Valenzuela Pardo <vlzdavid12@outlook.com>
2023-03-27 01:13:20 -0500

    Update Details

--- src/pages/details/[id].tsx
+++ src/pages/details/[id].tsx
@@ -21,6 +21,7 @@ const Details = ({movie}: any) => {
                         <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
                             {movie.name}
                         </h1>
+                        <p className="py-5 text-gray-500"><strong>Genero:</strong> {movie.gender}  <strong>Tipo:</strong> {movie.type} </p>
                         <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">From
                             checkout to global sales tax compliance, companies around the world use Flowbite to simplify
                             their payment stack.</p>
